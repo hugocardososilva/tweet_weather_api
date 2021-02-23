5b9df9dac75cba69584fa5d5a261bcb5344f03ca
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 18:10:22 -0300

    test(user): update factories

---
+++ spec/factories/settings.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :setting do
+    openwather_key { "ASD3248sfdfsdfs98yady983y9as8yd98asyd" }
+    twitter_api_key { "sabsad9823yr9hsdf9s8fh98shdf89hsdfd8sh" }
+    lang { Setting.langs.values.sample }
+    units { Setting.units.values.sample }
+
+    association :user
+
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    email { "MyString" }
-    name { "MyString" }
-    password { "MyString" }
+    username { FFaker::Internet.user_name }
+    email { FFaker::Internet.email }
+    name { FFaker::NameBR.name }
+    password { rand 99999999 }
+
+    after :create do |user|
+      FactoryBot.create(:setting, user: user)
+    end
   end
 end
