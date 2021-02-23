b8ed247fe1ea6f0b92a55c003b666200b638aa52
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 18:02:24 -0300

    chore(setting): remove unused files

--- spec/factories/api/v1/settings.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :api_v1_setting, class: 'Api::V1::Setting' do
-    openwather_key { "MyString" }
-    twitter_api_key { "MyString" }
-    lang { "MyString" }
-    metric { "MyString" }
-  end
-end
