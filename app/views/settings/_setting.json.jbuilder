3855fe9ed114adc9304202f56746eeb8210bb75f
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 01:56:34 -0300

    fix(setting): remove old folder

--- app/views/settings/_setting.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! setting, :id, :openwather_key, :twitter_api_key, :lang, :metric, :created_at, :updated_at
-json.url setting_url(setting, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :settings
+    end
+  end
   resources :settings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
