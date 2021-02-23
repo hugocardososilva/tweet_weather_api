8ff884221862404a084c075efb8a8f4e8a2f9f20
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 02:51:35 -0300

    build(rspec):add

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-    end
-  end
-  namespace :api do
-    namespace :v1 do
       resources :settings
     end
   end
-  resources :settings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
