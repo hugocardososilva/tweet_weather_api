0be3d283aca788a3cc4e1f897258c49adf89d7ef
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 17:42:29 -0300

    build(database): update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_045835) do
+ActiveRecord::Schema.define(version: 2021_02_23_061628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 2021_02_23_045835) do
     t.string "openwather_key"
     t.string "twitter_api_key"
     t.string "lang"
-    t.string "metric"
+    t.string "units"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_settings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_02_23_045835) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "settings", "users"
 end
