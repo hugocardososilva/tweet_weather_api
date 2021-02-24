e2b64d7a0ea38b02df12be441182d0cc4f84d5c5
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 21:44:36 -0300

    feat(tweet): add simple scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :settings
+      resources :tweets
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
