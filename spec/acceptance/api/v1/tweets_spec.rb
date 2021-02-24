e362a0a33f5afff463684d5874382602a3959f73
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 23:23:41 -0300

    test(tweet): add acceptance test

---
+++ spec/acceptance/api/v1/tweets_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'acceptance_helper'
+require 'rails_helper'
+
+def tweet_params
+  parameter :status, type: :array, items: { type: :string, enum: Tweet.statuses.values }, parent: 'tweet'
+  parameter :message, type: :test, parent: 'tweet', example: FFaker::Lorem.characters(140)
+  parameter :user_id, type: :integer, parent: 'tweet', example: 1, required: true
+end
+
+def tweet_attributes
+  let(:status) { "done" }
+  let(:message) { FFaker::Lorem.characters(140) }
+  let(:user_id) { FactoryBot.create(:user).id }
+end
+
+resource 'Tweet' do
+  explanation 'Tweet API reference '
+
+  header 'Accept', 'application/json'
+  header 'Content-Type', 'application/json'
+  header 'Host', 'localhost:3000'
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:tweet) { FactoryBot.create(:tweet, user: user) }
+
+  before do
+    2.times do |_t|
+      FactoryBot.create(:tweet, user: FactoryBot.create(:user))
+    end
+  end
+
+  # example object for show, update and destroy
+  let(:tweet) do
+    FactoryBot.create(:tweet, user: FactoryBot.create(:user))
+  end
+
+  # object id alias
+  let(:id) { tweet.id }
+
+
+  # index
+  get 'api/v1/tweets' do
+    # simulates a request
+    example_request 'Index' do
+      expect(response_status).to eq(200)
+    end
+  end
+
+  # show
+  get 'api/v1/tweets/:id' do
+    # simulates a request
+    example_request 'Show' do
+      expect(response_status).to eq(200)
+    end
+  end
+
+  # create
+  post 'api/v1/tweets' do
+    tweet_params
+
+    tweet_attributes
+
+    let(:raw_post) { { tweet: params }.to_json }
+
+    # simulates a request
+    example_request 'Create' do
+      expect(response_status).to eq(201)
+    end
+  end
+end
