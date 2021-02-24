6b8d77797d0ccbf402dcb2866979fe9fdea62857
Hugo Silva <hugocardososilva@gmail.com>
2021-02-24 00:46:26 -0300

    test(tweet): fix tests

--- spec/acceptance/api/v1/tweets_spec.rb
+++ spec/acceptance/api/v1/tweets_spec.rb
@@ -6,14 +6,14 @@ require 'rails_helper'
 def tweet_params
   parameter :status, type: :array, items: { type: :string, enum: Tweet.statuses.values }, parent: 'tweet'
   parameter :message, type: :text, parent: 'tweet', example: FFaker::Lorem.characters(140), required: true
-  parameter :tweet, type: :text, parent: 'tweet', example: FFaker::Lorem.AddressBR.city, required: true
+  parameter :location, type: :text, parent: 'tweet', example: FFaker::AddressBR.city, required: true
   parameter :user_id, type: :integer, parent: 'tweet', example: 1, required: true
 end
 
 def tweet_attributes
   let(:status) { 'done' }
   let(:message) { FFaker::Lorem.characters(140) }
-  let(:tweet) { FFaker::Lorem.AddressBR.city }
+  let(:location) { FFaker::AddressBR.city }
   let(:user_id) { FactoryBot.create(:user).id }
 end
 
