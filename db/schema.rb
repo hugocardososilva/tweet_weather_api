604d9c31a206c415ac1491d8803f9c8b95a9af3a
Hugo Silva <hugocardososilva@gmail.com>
2021-02-23 22:56:41 -0300

    build(ci): update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_004147) do
+ActiveRecord::Schema.define(version: 2021_02_24_015210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_02_24_004147) do
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_02_24_004147) do
   end
 
   add_foreign_key "settings", "users"
+  add_foreign_key "tweets", "users"
 end
