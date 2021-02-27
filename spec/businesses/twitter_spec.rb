7ee6167f822680b7eb84270b3d2b15728d522cbf
Hugo Silva <hugocardososilva@gmail.com>
2021-02-27 20:18:37 -0300

    test(tweet): remove unused code

---
+++ spec/businesses/twitter_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require_relative '../../lib/twitter_api/twitter_api'
+
+RSpec.describe "Twitterize" do
+
+  before (:all) do
+    @user = FactoryBot.create(:user, :with_good_keys)
+    @tweet = FactoryBot.build(:tweet, user: @user)
+    @client = TwitterApi.new({tweet: @tweet}).client
+  end
+
+  context 'send' do
+
+    it 'check send' do
+      @tweet.save!
+
+      expect(@tweet.status).to eq('done')
+    end
+  end
+end
