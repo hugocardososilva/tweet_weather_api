eb0a9834441cd09ac798f64d6bfd30f47019b6cb
Hugo Silva <hugocardososilva@gmail.com>
2021-02-27 19:52:05 -0300

    feat(tweet): add BO object

---
+++ app/businesses/twitterize.rb
@@ -0,0 +1,28 @@
+require_relative '../../lib/twitter_api/twitter_api'
+
+class Twitterize
+  def initialize(options = {})
+    @options = options
+    @params = @options[:params]
+    @tweet = @options[:tweet]
+  end
+
+  def send_twitter
+    message = send_message(@tweet.message)
+    if message
+      @tweet.update(status: "done")
+    else
+      @tweet.update(status: "error")
+    end
+  end
+
+  private
+
+  def send_message(message)
+    twitter_api.new({ tweet:@tweet }).send_twitter message
+  end
+
+  def twitter_api
+    TwitterApi
+  end
+end
