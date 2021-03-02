625003e9b14d8d036d3a0da119e81eb4eac41acf
Hugo Silva <hugocardososilva@gmail.com>
2021-03-02 13:34:12 -0300

    feat(tweet): add rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from Exception, with: :catching_errors
+
+  def record_not_found
+    render json: { errors: { base: ['Error'] } }, status: :unprocessable_entity
+  end
 end
